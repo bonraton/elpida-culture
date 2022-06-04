ee162106dbba8ffe4f7f5c90bd5a81b4941d1402
bonraton <bonraton@yandex.ru>
2022-06-04 19:46:06 +0300

    feat: Header

--- src/src/components/App/App.jsx
+++ src/src/components/App/App.jsx
@@ -1,5 +1,14 @@
-export const App = () => {
-    <div className="app">
+import React from 'react';
+// import { Routes } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
+import Header from '../Header';
 
-    </div>
+export default function App() {
+  return (
+    <BrowserRouter>
+      <div className="app">
+        <Header />
+      </div>
+    </BrowserRouter>
+  );
 }
---
+++ src/src/components/Header.jsx
@@ -0,0 +1,20 @@
+import { NavLink } from 'react-router-dom';
+import { React } from 'react';
+import logo from '../images/logos/logo_dark.svg';
+
+export default function Header() {
+  return (
+    <header className="header">
+      <img className="logo" src={logo} alt="logo" />
+      <nav>
+        <ul className="navigation">
+          <li><NavLink className={(navData) => (navData.isActive ? 'link link_active' : 'link')} to="/about">About</NavLink></li>
+          <li><NavLink className={(navData) => (navData.isActive ? 'link link_active' : 'link')} to="/refuges">Refugees</NavLink></li>
+          <li><NavLink className={(navData) => (navData.isActive ? 'link link_active' : 'link')} to="/membership">Membership</NavLink></li>
+          <li><NavLink className={(navData) => (navData.isActive ? 'link link_active' : 'link')} to="/donations">Donations</NavLink></li>
+          <li><NavLink className={(navData) => (navData.isActive ? 'link link_active' : 'link')} to="/projects">Projects</NavLink></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
