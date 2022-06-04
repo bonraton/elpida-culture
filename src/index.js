0834494af6c161ad0fa7ec2805c1d1e45a6d404d
bonraton <bonraton@yandex.ru>
2022-06-04 17:26:28 +0300

    feat: reset, fonts

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from './src/components/App/App';
-
+import App from './src/components/App/App';
+import './index.scss';
+//
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  // eslint-disable-next-line react/jsx-filename-extension
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
-
