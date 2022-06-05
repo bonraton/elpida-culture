cc327de5665c8d174113e8ea8a96cd42fd65fb19
bonraton <bonraton@yandex.ru>
2022-06-05 22:08:12 +0300

    feat: card, actions component

--- src/src/components/Main.jsx
+++ src/src/components/Main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Promo from './Main/Promo';
 import Header from './Header';
+import Actions from './Main/Actions';
 
 export default function Main() {
   return (
     <main className="Main">
       <Header />
       <Promo />
+      <Actions />
     </main>
   );
 }
