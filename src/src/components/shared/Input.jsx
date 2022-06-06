07ebca496a90fc659572c05caff0b850460d42be
bonraton <bonraton@yandex.ru>
2022-06-06 13:32:31 +0300

    feat: Input component

---
+++ src/src/components/shared/Input.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function Input(props) {
+  const {
+    className, labelClass, name, placeholder, label,
+  } = props;
+  return (
+    <label htmlFor={name} className={labelClass}>
+      <span className="input__error">ERORR MAZAFAKA</span>
+      {label}
+      <input name={name} className={className} placeholder={placeholder} />
+    </label>
+  );
+}
+
+Input.propTypes = {
+  className: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  placeholder: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  labelClass: PropTypes.string.isRequired,
+};
