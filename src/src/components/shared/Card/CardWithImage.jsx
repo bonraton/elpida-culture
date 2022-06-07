1e288f078bec87cbad93e5e46c0627988e170472
bonraton <bonraton@yandex.ru>
2022-06-07 15:19:26 +0300

    feat: cardWithImage

---
+++ src/src/components/shared/Card/CardWithImage.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+export default function CardWithImage(props) {
+  const [textVisible, setTextVisible] = useState(true);
+  const textClass = !textVisible ? 'card__text' : 'card__text card__text_hidden';
+  function handleClick() {
+    setTextVisible(!textVisible);
+  }
+  const {
+    title, subtitle, image, text,
+  } = props;
+  return (
+    <div className="card card_image">
+      <img className="card__image" alt="someAlt" src={image} />
+      <h4 className="card__title">{title}</h4>
+      <p className="card__subtitle">{subtitle}</p>
+      <p className={textClass}>{text}</p>
+      <button onClick={handleClick} type="button" className="card__button">
+        Read more &#32;&darr;
+      </button>
+    </div>
+  );
+}
+
+CardWithImage.propTypes = {
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+};
